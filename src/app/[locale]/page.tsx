cda5affcfc6c32f1952d936b888c3b7f1157b1e0
D-SZAdorjan <demenyador@gmail.com>
2024-08-13 15:04:04 +0300

    @fortawesome/fontawesome-svg-core npm package installed, @fortawesome/free-brands-svg-icons npm package installed, @fortawesome/free-regular-svg-icons npm package installed,  @fortawesome/free-solid-svg-icons npm package installed, @fortawesome/react-fontawesome npm package installed, FontAwesome icons set up

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,8 @@
 import { getIntl } from "@/i18n";
+import { faMeta, faXbox } from "@fortawesome/free-brands-svg-icons";
+import { faHourglass, faHourglass2, faHourglassEmpty, faSmile } from "@fortawesome/free-regular-svg-icons";
+import { faHourglass1, faHourglass3,  faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 
 export default async function Home({params} : {
@@ -12,7 +16,9 @@ export default async function Home({params} : {
 
   return (
     <main className="flex h-full w-full items-center justify-center">
-      <h2 className="h-fit text-3xl text-orange-500">{translations.messages.indexPageText as string}</h2>
+      <h2 className="h-fit text-3xl text-orange-500 flex gap-x-3">
+        {translations.messages.indexPageText as string}
+        <FontAwesomeIcon icon={faMeta}/></h2>
     </main>
   );
 }
